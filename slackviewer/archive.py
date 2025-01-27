2e2351ce1b8994dbc1e5bf97be40ee4b686c9239
Chinchien Lin <lin810116@gmail.com>
2025-01-28 11:15:13 +1300

    feat: download files

--- slackviewer/archive.py
+++ slackviewer/archive.py
@@ -6,11 +6,27 @@ import io
 
 from os.path import basename, splitext
 
+from pathlib import Path
 import requests
 
 import slackviewer
 from slackviewer.constants import SLACKVIEWER_TEMP_PATH
 from slackviewer.utils.six import to_unicode, to_bytes
+
+from urllib.parse import urlparse
+
+
+def truncate_path(path):
+    # max length for path separator between parent and name: 2 (Windows)
+    max_len = 260 - 2
+
+    if len(str(path)) > max_len:
+        stem_len = max_len - len(str(path.parent)) - len(path.suffix)
+        filename_truncated = path.stem[:stem_len]
+        new_path = path.parent.joinpath(filename_truncated + path.suffix)
+        path = new_path
+
+    return Path(path)
 
 def download_file(url, local_file):
     # Send an HTTP request to the given URL
@@ -24,6 +40,16 @@ def download_file(url, local_file):
                 f.write(chunk)
 
     return local_file
+
+
+def get_all_json_files_recursively(directory):
+    json_file_list = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.json'):
+                json_file_list.append(os.path.join(root, file))
+    return json_file_list
+
 
 def SHA1_file(filepath, extra=b''):
     """
@@ -46,6 +72,60 @@ def SHA1_file(filepath, extra=b''):
 
 
 def extract_archive(filepath):
+def download_files(data, files, item_idx, dest_dir):
+    """
+    Downloads & saves files
+
+    :param data: The data containing the files information.
+    :type data: list
+    :param files: The list of files to be downloaded.
+    :type files: list
+    :param item_idx: The index of the current item in the data list.
+    :type item_idx: int
+    :param dest_dir: The destination directory where files will be saved.
+    :type dest_dir: pathlib.Path
+    """
+    dest_dir = dest_dir.joinpath("files")
+    dest_dir.mkdir(exist_ok=True)
+
+    if not files:
+        return
+
+    for file_idx, file in enumerate(files):
+        is_external = file.get("is_external")
+        if is_external:
+            continue
+
+        mode = file.get("mode")
+        if mode and mode in ["hidden_by_limit", "tombstone"]:
+            continue
+
+        name = file.get("name")
+
+        user_team = file.get("user_team")
+        id = file.get("id")
+        file_id = user_team + "-" + id
+
+        # url_fields = ["url_private", "url_private_download", "thumb_pdf", "permalink", "permalink_public"]
+        url_fields = ["url_private", "url_private_download", "thumb_pdf"]
+
+        for url_field in url_fields:
+            url = file.get(url_field)
+            if url:
+                file_path = dest_dir.joinpath(file_id,  url_field, name)
+
+                file_path = truncate_path(file_path)
+
+                print("file_path: " + str(file_path))
+                file_path.parent.mkdir(parents=True, exist_ok=True)
+                download_file(url, str(file_path))
+                # replace remote url with local url
+                base_path = file_path.parent.parent.parent.parent.parent
+                file_path_relative = file_path.relative_to(base_path)
+                data[item_idx]["files"][file_idx][url_field] = str(file_path_relative)
+
+                print("Remote URL - " + url_field + ": " + str(url))
+                print("local URL: " + str(file_path_relative))
 def extract_archive(filepath, extracted_path=None):
     """
     Returns the path of the archive
@@ -100,6 +180,29 @@ def extract_archive(filepath, extracted_path=None):
 
         # Add additional file with archive info
         create_archive_info(filepath, extracted_path, archive_sha)
+
+    # download
+    dest_dir = Path(extracted_path)
+    file_dir = dest_dir.joinpath("files")
+    file_dir.mkdir(exist_ok=True)
+
+    # excluded_files = [".slackviewer_archive_info.json", "canvases.json", "channels.json", "huddle_transcripts.json", "integration_logs.json", "lists.json", "users.json"]
+    excluded_files = [".slackviewer_archive_info.json", "canvases.json", "channels.json", "huddle_transcripts.json",
+                      "integration_logs.json", "lists.json"]
+    json_files = get_all_json_files_recursively(extracted_path)
+    for json_file in json_files:
+        if any(excluded_file in json_file for excluded_file in excluded_files):
+            continue
+        print("processing " + str(json_file))
+        with open(json_file, "r", encoding="cp866") as f:
+            data = json.load(f)
+
+
+        for item_idx, item in enumerate(data):
+            download_files(data, item.get("files"), item_idx, file_dir)
+
+        with open(json_file, "w") as f:
+            json.dump(data, f)
 
     return extracted_path
 
